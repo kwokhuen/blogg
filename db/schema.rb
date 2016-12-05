6eeb38549233dd6034943be6ea98077f3669ee8c
kwokhuen <benjaminkwokhuen@gmail.com>
2016-12-05 03:12:57 -0800

    add user sign up page, add user table, add association between user and article, add style to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204042646) do
+ActiveRecord::Schema.define(version: 20161205091402) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       null: false
     t.text     "description", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
